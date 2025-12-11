f94e211dab78e74b09af6243fa1c9949ddb3c686
karextokenmining-dot <karextokenmining@gmail.com>
2025-12-11 21:36:31 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+import TelegramBot from "node-telegram-bot-api";
+import http from "http";
+
+// ====== BOT TOKENİNİ BURAYA KOY KANKİ ======
+const BOT_TOKEN = process.env.BOT_TOKEN || "8284256760:AAE1CMFjJZTyN6BOonubmCIsu6JtPi-0vC4";
+
+// ====== BOTU BAŞLAT ======
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+console.log("KAREX Mining bot çalışıyor 🔥");
+
+// ====== /start KOMUTU ======
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const webAppUrl = "https://karex-mining-bot-1.onrender.com"; // BURAYA KENDİ WEBAPP LINKIN GELECEK
+
+  bot.sendMessage(
+    chatId,
+    "KAREX Mining'e hoş geldin kanki! 🔥\nAşağıdaki butondan uygulamayı aç:",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "🚀 KAREX APP'i Aç",
+              web_app: { url: webAppUrl },
+            },
+          ],
+        ],
+      },
+    }
+  );
+});
+
+// ====== SADECE RENDER STOP ETMESİN DİYE PORT AÇIYORUZ ======
+http
+  .createServer((req, res) => {
+    res.end("KAREX bot çalışıyor.");
+  })
+  .listen(process.env.PORT || 8080);
